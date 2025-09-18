e075c0a074ec48e90d7e183b4c38dea50edeaa4d
alinur229035@gmail.com <alinur229035@gmail.comgit config --global user.email alinur229035@gmail.com>
2025-09-18 18:18:21 +0500

    base

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = new int[10];
-        for (int i = 0; i < array.length; i++) {
-            array[i] = (int)(Math.random()*100);
-        }
-        System.out.println(Arrays.toString(array));
-        MergeSort.mergeSort(array);
-        System.out.println(Arrays.toString(array));
+
      }
 }
